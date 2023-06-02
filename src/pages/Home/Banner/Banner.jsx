421adf9881ff8a95594a17a72e9b5a40b3e7266d
momenur <momenurislam6@gmail.com>
2023-06-02 11:31:22 +0600

    home pHoto

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -4,19 +4,19 @@ import './Banner.css'
 
 // Slider Image
 
-// import s1 from '../../../assets/home/01.jpg'
-// import s2 from '../../../assets/home/02.jpg'
-// import s3 from '../../../assets/home/03.png'
-// import s4 from '../../../assets/home/04.jpg'
-// import s5 from '../../../assets/home/05.png'
-// import s6 from '../../../assets/home/06.png'
+import s1 from '../../../assets/home/01.jpg'
+import s2 from '../../../assets/home/02.jpg'
+import s3 from '../../../assets/home/03.png'
+import s4 from '../../../assets/home/04.jpg'
+import s5 from '../../../assets/home/05.png'
+import s6 from '../../../assets/home/06.png'
 const Banner = () => {
-   const s1 ="https://ibb.co/JpknYZP" 
-   const s2 ="https://ibb.co/c6zHDdz" 
-   const s3 ="https://ibb.co/gV6bNk9" 
-   const s4 ="https://ibb.co/Zfxqkbs" 
-   const s5 ="https://ibb.co/tKTpkTx" 
-   const s6 ="https://ibb.co/bNXvFZB" 
+//    const s1 ="https://ibb.co/JpknYZP" 
+//    const s2 ="https://ibb.co/c6zHDdz" 
+//    const s3 ="https://ibb.co/gV6bNk9" 
+//    const s4 ="https://ibb.co/Zfxqkbs" 
+//    const s5 ="https://ibb.co/tKTpkTx" 
+//    const s6 ="https://ibb.co/bNXvFZB" 
     return (
         <div>
             <Carousel>
